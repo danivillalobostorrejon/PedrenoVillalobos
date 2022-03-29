12d8d07b1d9605dcfef51dea102fa7e2e3ab021d
danivillalobostorrejon <dani.villalobos@orderdata.com>
2022-03-29 19:47:16 +0200

    new fields

--- eci/items.py
+++ eci/items.py
@@ -8,5 +8,7 @@ class eciItem(scrapy.Item):
     brand = scrapy.Field()
     category = scrapy.Field()
     price = scrapy.Field()
+    price_discount = scrapy.Field()
     discount = scrapy.Field()
+    perc_discount = scrapy.Field()
     #colors = scrapy.Field()
